461d559e4c6de2a8c5b1f87a5e6e8d1000455b15
Caleb Korson <caleb@Calebs-MacBook-Air.local>
2023-01-10 22:04:03 -0500

    began working on models

--- server/models/User.js
+++ server/models/User.js
@@ -53,9 +53,9 @@ userSchema.methods.isCorrectPassword = async function (password) {
 };
 
 // when we query a user, we'll also get another field called `bookCount` with the number of saved books we have
-userSchema.virtual('bookCount').get(function () {
-  return this.savedBooks.length;
-});
+// userSchema.virtual('bookCount').get(function () {
+//   return this.savedBooks.length;
+// });
 
 const User = model('User', userSchema);
 
