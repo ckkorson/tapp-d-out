1e8d0afc2faea0cc76df70540bc2a252e8c4c581
Bradley Hambrick <bradleyhambrick@Bradleys-MacBook-Pro.local>
2023-01-17 20:35:40 -0600

    file structuring for front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+// import Header from "./components/Header/index";
+// import Footer from "./components/Footer/index";
+// import Profile from "./components/profile/index";
+import Login from "./components/Login/index";
+// import Newtab from "./components/Newtab/index";
+// import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("Home");
+
+  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  // const renderPage = () => {
+  //   if (currentPage === "Home") {
+  //     return <Profile />;
+  //   }
+  //   if (currentPage === "Login") {
+  //     return <Login />;
+  //   }
+  //   if (currentPage === "Newtab") {
+  //     return <Newtab />;
+  //   }
+  // };
+
+  const handlePageChange = (page) => setCurrentPage(page);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Header handlePageChange={handlePageChange} /> */}
+
+      <Login />
+
+      {/* {renderPage()} */}
+
+      {/* <Footer /> */}
     </div>
   );
 }
-
 export default App;
