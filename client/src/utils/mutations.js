b68cf19f41b1bc9fb009fbd1b963251c84d5ab32
Bradley Hambrick <bradleyhambrick@Bradleys-MacBook-Pro.local>
2023-01-19 21:11:11 -0600

    troubleshooting routes

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,19 +1,24 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
-export const LOGIN_USER = gql`
+export const LOGIN = gql`
   mutation login($username: String!, $password: String!) {
     login(username: $username, password: $password) {
-      token
       user {
         _id
         username
       }
+      token
     }
   }
 `;
 
 export const ADD_USER = gql`
-  mutation addUser($name: String!, $username: String!, $email: String!, $password: String!) {
+  mutation addUser(
+    $name: String!
+    $username: String!
+    $email: String!
+    $password: String!
+  ) {
     addUser(username: $username, email: $email, password: $password) {
       token
       user {
